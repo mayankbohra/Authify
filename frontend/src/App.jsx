46705d3df016af998bb02823274d87668257bfdd
mayankbohra <bohramayank4u@gmail.com>
2024-11-25 21:45:26 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import FloatingShape from './components/FloatingShape';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+    return (
+        <>
+            <div className='overflow-hidden min-h-screen bg-gradient-to-br from-indigo-800 via-black to-gray-900 flex items-center justify-center relative'>
+                <FloatingShape color='bg-cyan-400' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+                <FloatingShape color='bg-pink-400' size='w-48 h-48' top='70%' left='80%' delay={5} />
+                <FloatingShape color='bg-lime-400' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+
+                <Routes>
+                    <Router path='/' element={"Home"} />
+                    <Router path='/signup' element={<SignUpPage />} />
+                    <Router path='/login' element={<LoginPage />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
+
+export default App
