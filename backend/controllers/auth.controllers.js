a19aa6018ff5953eb45711ba389b24b361fa0ce8
mayankbohra <bohramayank4u@gmail.com>
2024-11-25 15:02:31 +0530

    create verification endpoint and email template

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 
 import { User } from "../models/user.model.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/email.js";
+import { sendVerificationEmail,  sendWelcomeEmail} from "../mailtrap/email.js";
 
 export const signup = async (req, res) => {
     const { email, password, name } = req.body;
@@ -44,12 +44,46 @@ export const signup = async (req, res) => {
     } catch (error) {
         res.status(400).json({ success: false, message: error.message });
     }
-}
+};
+
+export const verifyEmail = async (req, res) => {
+    const {code} = req.body;
+
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() },
+        });
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid or expired verification code" });
+        }
+
+        user.verified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+
+        await user.save();
+        await sendWelcomeEmail(user.email, user.name);
+
+        res.status(200).json({
+            success: true,
+            message: "Email verified successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            }
+        });
+    } catch (error) {
+        console.error("Error verifying email: ", error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+};
 
 export const login = async (req, res) => {
     res.send("Login Route");
-}
+};
 
 export const logout = async (req, res) => {
     res.send("Logout Route");
-}
+};
--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+
 import { VERIFICATION_EMAIL_TEMPLATE } from "./mailTemplate.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
+
+dotenv.config();
 
 export const sendVerificationEmail = async (email, verificationToken) => {
     const recipient = [{ email }];
@@ -18,5 +22,26 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         console.error(`Error sending verification`, error);
 
 		throw new Error(`Error sending verification email: ${error}`);
+    }
+}
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: process.env.MAILTRAP_API_TEMPLATE_UUID,
+            template_variables: {
+              "name":  name
+            }
+        });
+
+        console.log("Welcome email sent: ", response);
+    } catch (error) {
+        console.error(`Error sending welcome email`, error);
+
+        throw new Error(`Error sending welcome email: ${error}`);
     }
 }
